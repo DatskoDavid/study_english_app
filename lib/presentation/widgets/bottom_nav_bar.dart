b75fc35ca61107bb4ee1f2132b3de1758ed94c3f
Datsko David <datskodavid@gmail.com>
2024-03-24 23:54:24 +0200

    replace simple nav_bar with google_nav_bar

--- lib/presentation/widgets/bottom_nav_bar.dart
+++ lib/presentation/widgets/bottom_nav_bar.dart
@@ -1,42 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+import 'package:real_diploma/constants/colors.dart';
 
-class BottomNavBar extends StatelessWidget {
-  final int selectedIndex;
-  final Function(int) onTap;
-
-  const BottomNavBar({
-    required this.selectedIndex,
+class BottomNavbar extends StatelessWidget {
+  const BottomNavbar({
+    super.key,
     required this.onTap,
-    Key? key,
-  }) : super(key: key);
+    required this.selectedIndex,
+  });
+
+  final Function(int) onTap;
+  final int selectedIndex;
 
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(
-      currentIndex: selectedIndex,
-      onTap: onTap,
-      type: BottomNavigationBarType.fixed,
-      selectedItemColor: Colors.black,
-      unselectedItemColor: Colors.grey,
-      showUnselectedLabels: true,
-      items: const <BottomNavigationBarItem>[
-        BottomNavigationBarItem(
-          icon: Icon(Icons.home),
-          label: 'Home',
+    return Container(
+      decoration: BoxDecoration(
+        color: AppColors.background,
+        // color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            blurRadius: 20,
+            color: Colors.black.withOpacity(.1),
+          )
+        ],
+      ),
+      child: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 8),
+          child: GNav(
+            rippleColor: Colors.grey[300]!,
+            hoverColor: Colors.grey[100]!,
+            gap: 8,
+            activeColor: AppColors.primaryColor,
+            iconSize: 24,
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+            duration: const Duration(milliseconds: 400),
+            tabBackgroundColor: AppColors.white,
+            // tabBackgroundColor: Colors.grey[100]!,
+            color: AppColors.primaryColor,
+            tabs: const [
+              GButton(
+                icon: Icons.home,
+                text: 'Home',
+              ),
+              GButton(
+                icon: Icons.book,
+                text: 'Vocabulary',
+              ),
+              GButton(
+                icon: Icons.ballot,
+                text: 'Training',
+              ),
+              GButton(
+                icon: Icons.person,
+                text: 'Profile',
+              ),
+            ],
+            selectedIndex: selectedIndex,
+            onTabChange: onTap,
+          ),
         ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.book),
-          label: 'Vocabulary',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.ballot),
-          label: 'Training',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.person),
-          label: 'Profile',
-        ),
-      ],
+      ),
     );
   }
 }
